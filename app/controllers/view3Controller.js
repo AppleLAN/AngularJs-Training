3cebdafeb465ff41cd34a792990ad254a686ede1
Rafael Pérez García <info@imperdiblesoft.com>
2015-05-29 20:30:58 -0300

    Code fixing

--- app/controllers/view3Controller.js
+++ app/controllers/view3Controller.js
@@ -1,26 +1,69 @@
 app.controller("view3Controller", ['$scope', function($scope) {
-  	{    
-		var Persona = function(nombre, apellido) {
-			var self = this;
-			this.nombre = nombre;
-			this.apellido = apellido;
-		};
-		var Dog = function(dogName, pedigree) {
-			var self = this;
-			this.dogName = dogName;
-			this.pedigree = pedigree;
-		};
-		$scope.messagePersona = "persona";
-		$scope.messagePerro = "perro";
-		$scope.list = new Array();
-		$scope.createPerson = function(){
-			var personObject = new Persona($scope.userName,$scope.userLastName);
-			$scope.list.push(personObject);
-		}
-		$scope.createDog = function(dogName,pedigree){
-			var dogObject = new Dog(dogName,pedigree);
-			$scope.list.push(dogObject);
-		}
+	
+	/*	Initial Variables	*/
+	$scope.messagePersona = "persona";
+	$scope.messagePerro = "perro";
+	
+	/*	Create scope objects	*/
+	$scope.person = {
+		"name": "",
+		"lastName": ""
 	}
-}]
-);
+	$scope.animal = {
+		"name": "",
+		"pedigree": ""
+	}
+	
+	$scope.list = new Array();
+	
+	/*	Methods declarations	*/
+	/*	Create new Person object	*/
+	$scope.createPerson = function(){
+		var newPerson = new Persona($scope.person.name, $scope.person.lastName);
+		$scope.list.push(newPerson);
+		$scope.clearForm();
+	}
+	
+	/*	Create new Animal object	*/
+	$scope.createDog = function(){
+		var newDog = new Dog($scope.animal.name, $scope.animal.pedigree);
+		$scope.list.push(newDog);
+		$scope.clearForm();
+	}
+	
+	$scope.clearForm = function(){
+		$scope.person = {
+			"name": "",
+			"lastName": ""
+		}
+		$scope.animal = {
+			"name": "",
+			"pedigree": ""
+		}
+		$scope.checked1 = false;
+		$scope.checked2 = false;
+	}
+	
+	/*	Classes declarations	*/
+	/*	Class Persona	*/
+	var Persona = function(nombre, apellido) {
+		var self = this;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		
+		this.saludar = function(){
+			return "Hola, soy "+ self.nombre +" "+ self.apellido +".";
+		}
+	};
+	
+	/*	Class Dog	*/
+	var Dog = function(nombre, pedigree) {
+		var self = this;
+		this.nombre = nombre;
+		this.pedigree = pedigree;
+		
+		this.saludar = function(){
+			return "Woof, soy "+ self.nombre +", y soy un "+ self.pedigree +", woof.";
+		}
+	};
+}]);
